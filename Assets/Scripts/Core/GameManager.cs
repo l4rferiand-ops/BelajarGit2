42f6e39ec5ffdce8d67945b33ad6e599d2177b1c
dylanjkl <l4rferiand@gmail.com>
2026-04-23 18:45:18 +0700

    fix: implement pause and resume state

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,38 +1,103 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public GameState currentState { get; private set; }
+    private UnityEvent<GameState> OnStateChanged;
 
-    public GameState currentState;
 
     void Awake()
     {
-        Instance = this;
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
     }
 
     void Start()
     {
-        currentState = GameState.Playing;
+        UpdateState(GameState.MainMenu);
     }
 
+    public void UpdateState(GameState newState)
+    {
+        currentState = newState;
+
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                OnMainMenu();
+                break;
+            case GameState.Playing:
+                OnPlaying();
+                break;
+            case GameState.Paused:
+                PauseGame();
+                break;
+            case GameState.GameOver:
+                GameOver();
+                break;
+
+        }
+        OnStateChanged?.Invoke(newState);
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (currentState == GameState.Paused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Resume();
+        }
+        else if (currentState != GameState.Paused && Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
         }
+        else if (currentState == GameState.GameOver && Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
     }
 
+    private void OnMainMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Kembali ke menu");
+    }
+    private void OnPlaying()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Game Started");
+    }
     public void PauseGame()
     {
         Time.timeScale = 0f;
+        Debug.Log("Game Paused");
         currentState = GameState.Paused;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Game Resumed");
+        currentState = GameState.Playing;
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Game Restarted");
+        StartGame();
     }
 
     public void GameOver()
     {
+        Time.timeScale = 0f;
         Debug.Log("Game Over");
         currentState = GameState.GameOver;
     }
+    public void StartGame() => UpdateState(GameState.Playing);
+    public void OnPause() => UpdateState(GameState.Paused);
+    public void ResumeGame() => UpdateState(GameState.Playing);
+    public void Over() => UpdateState(GameState.GameOver);
 }
